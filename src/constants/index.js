2e68b9e60ee9cb16df3853a5b3e1878e8176bb4a
saksham-rejoice <saksham369.rejoice@gmail.com>
2025-12-30 18:17:32 +0530

    changes done

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,8 @@
 import shapingConfident from "./shaping-confident.json";
-
-
+import topCourses from "./top-courses.json";
+import whyChoose from "./whychoose.json";
 const shapingConfidentData = shapingConfident;
+const topCoursesData = topCourses;
+const whyChooseData = whyChoose;
 
-export {shapingConfidentData};
+export { shapingConfidentData, topCoursesData,whyChooseData };
